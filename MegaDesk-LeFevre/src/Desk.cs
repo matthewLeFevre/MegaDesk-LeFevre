56f50bec226b16c91637df354e0d26061d7ac929
Matthew LeFevre <matthewlefevre95@gmail.com>
2019-05-11 20:19:41 -0600

    Add project files.

---
+++ MegaDesk-LeFevre/src/Desk.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_LeFevre.src
+{
+    public enum DeskMaterial :byte
+    {
+        Pine        = 0,
+        Laminate    = 1,
+        Veneer      = 2,
+        Oak         = 3,
+        Rosewood    = 4
+    }
+    public class Desk
+    {
+        private int          Width;
+        private int          Depth;
+        private int          DrawerCount;
+        private DeskMaterial Material;
+
+        public Desk(int isWidth, int isDepth, int isDrawerCount, DeskMaterial isMaterial)
+        {
+            this.Width       = isWidth;
+            this.Depth       = isDepth;
+            this.DrawerCount = isDrawerCount;
+            this.Material    = isMaterial;
+        }
+
+        public int getSurfaceArea()
+        {
+            return this.Width * this.Depth;
+        }
+
+        public int getDrawerCount()
+        {
+            return this.DrawerCount;
+        }
+
+        public DeskMaterial getMaterial()
+        {
+            return this.Material;
+        }
+
+        public static int getMaterialPrice (DeskMaterial material)
+        {
+            int materialPrice;
+            switch (material.ToString())
+            {
+                case "Pine":
+                    materialPrice = 50;
+                    break;
+                case "Laminate":
+                    materialPrice = 100;
+                    break;
+                case "Veneer":
+                    materialPrice = 125;
+                    break;
+                case "Oak":
+                    materialPrice = 200;
+                    break;
+                case "Rosewood":
+                    materialPrice = 300;
+                    break;
+                default:
+                    // Throw an error
+                    materialPrice = 999;
+                    break;
+            }
+
+            return materialPrice;
+        }
+    }
+}
---
+++ MegaDesk-LeFevre/src/DeskQuote.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_LeFevre.src
+{
+    public enum ProductionMethod : byte
+    {
+        normal      = 0,
+        rush3day    = 1,
+        rush5day    = 2,
+        rush7day    = 3
+    }
+    public class DeskQuote
+    {
+        private int              QuotePrice;
+        private string           CustomerName;
+        private ProductionMethod ProductionMethod;
+        private Desk             Desk;
+
+        public DeskQuote() { }
+
+        public int getQuote()
+        {
+            const int BaseFee = 200;
+            int materialPrice = Desk.getMaterialPrice(this.Desk.getMaterial());
+            int orderPrice = DeskQuote.getOrderPrice(this.ProductionMethod, this.Desk.getSurfaceArea());
+            int drawerPrice = this.Desk.getDrawerCount() * 30;
+
+
+            int finalPrice = materialPrice + orderPrice + drawerPrice + BaseFee;
+            return finalPrice;
+        }
+        public static int getOrderPrice(ProductionMethod method, int sizeOfDesk)
+        {
+            int orderPrice;
+
+            switch(method.ToString())
+            {
+                case "rush3day":
+                    if(sizeOfDesk < 1000)
+                    {
+                        orderPrice = 60;
+                    }
+                    else if (sizeOfDesk > 1000 && sizeOfDesk < 2000)
+                    {
+                        orderPrice = 70;
+                    } else
+                    {
+                        orderPrice = 80;
+                    }
+                    break;
+                case "rush5day":
+                    if (sizeOfDesk < 1000)
+                    {
+                        orderPrice = 40;
+                    }
+                    else if (sizeOfDesk > 1000 && sizeOfDesk < 2000)
+                    {
+                        orderPrice = 50;
+                    }
+                    else
+                    {
+                        orderPrice = 60;
+                    }
+                    break;
+                case "rush7day":
+                    if (sizeOfDesk < 1000)
+                    {
+                        orderPrice = 30;
+                    }
+                    else if (sizeOfDesk > 1000 && sizeOfDesk < 2000)
+                    {
+                        orderPrice = 35;
+                    }
+                    else
+                    {
+                        orderPrice = 40;
+                    }
+                    break;
+                default:
+                    orderPrice = 0;
+                    break;
+            }
+
+            return orderPrice;
+        }
+    }
+}
